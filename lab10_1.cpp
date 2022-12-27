aa16535bfa0c68dad4fce2fcdf3d6824868b61c3
Natan <th3than@gmail.com>
2022-12-27 15:47:03 +0700

    lab10-1

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -3,9 +3,15 @@
 using namespace std;
 
 int main(){	
+	double loan,rate,rateP,pay,total;
 	cout << "Enter initial loan: ";
+	cin >> loan;
+	total = loan;
 	cout << "Enter interest rate per year (%): ";
+	cin >> rate;
+	rate /= 100;
 	cout << "Enter amount you can pay per year: ";
+	cin >> pay;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -21,13 +27,27 @@ int main(){
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	int i = 1;
+	do
+	{
+		cout << setw(13) << left << i; 
+		cout << setw(13) << left << total;
+		rateP = rate*total;
+		cout << setw(13) << left << rateP;
+		total += rateP;
+		cout << setw(13) << left << total;
+		if (total-pay <= 0)
+		{
+			pay = total;
+			total = pay-total;
+		}else{
+			total -= pay;
+		}
+		cout << setw(13) << left << pay;
+		cout << setw(13) << left << total;
+		i++;
+		cout << "\n";
+	} while (total != 0 && total > 0);
 	
 	return 0;
 }
